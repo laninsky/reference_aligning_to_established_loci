85470f62dfdae41df3d18529b9df14551a1893de
Alana Alexander <laninsky@gmail.com>
2016-02-03 20:20:53 -0600

    Create summarize_coverage.R

---
+++ uce_version/summarize_coverage.R
@@ -0,0 +1,14 @@
+intable <- read.table("coverage_summary.txt",header=FALSE,stringsAsFactors=FALSE,sep=" ")
+locinames <- read.table("fasta_names",header=FALSE,stringsAsFactors=FALSE,sep=" ")
+
+noloci <- dim(locinames)[1]
+
+outtable <- matrix(NA,ncol=3,nrow=noloci)
+outtable[,1] <- locinames[,1]
+
+for (i in 1:noloci) {
+outtable[,2] <- sum(intable[,2]==locinames[i,1])
+outtable[,3] <- mean(as.numeric(intable[(which(intable[,2]==locinames[i,1])),7]))
+}
+
+write.table(outtable, "locus_coverage_data.txt",quote=FALSE, col.names=FALSE,row.names=FALSE)

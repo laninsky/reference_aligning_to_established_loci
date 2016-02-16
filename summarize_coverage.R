36fa8798b3a51feb2c68a362d19978811d991d6b
Alana Alexander <laninsky@gmail.com>
2016-02-16 09:26:19 -0600

    Update summarize_coverage.R

--- summarize_coverage.R
+++ summarize_coverage.R
@@ -1,14 +1,26 @@
 intable <- read.table("coverage_summary.txt",header=FALSE,stringsAsFactors=FALSE,sep=" ")
 locinames <- read.table("fasta_names",header=FALSE,stringsAsFactors=FALSE,sep=" ")
+samplenames <- read.table("samples.txt",header=FALSE,stringsAsFactors=FALSE,sep=" ")
 
 noloci <- dim(locinames)[1]
+nosamples <- dim(samplenames)[1]
 
-outtable <- matrix(NA,ncol=3,nrow=noloci)
+outtable <- matrix(NA,ncol=(3+(2*nosamples)),nrow=noloci)
 outtable[,1] <- locinames[,1]
 
 for (i in 1:noloci) {
 outtable[i,2] <- sum(intable[,2]==locinames[i,1])
 outtable[i,3] <- mean(as.numeric(intable[(which(intable[,2]==locinames[i,1])),7]))
+for (j in 1:nosamples) {
+k <- which((intable[,2]==locinames[i,1]) & (intable[,1]==samplenames[j,1]))
+if(length(k)>0) {
+outtable[i,(j+3)] <- intable[k,8]
+outtable[i,(nosamples+j+3)] <- as.numeric(intable[k,4])/as.numeric(intable[k,3])
 }
+}
+}
+
+titlerow <- c("locus","no_of_samples","av_cov_across_samples",paste(samplenames,"_av_cov",sep=""),paste(samplenames,"prop_locus_covered",sep=""))
+outtable <- rbind(titlerow,outtable)
 
 write.table(outtable, "locus_coverage_data.txt",quote=FALSE, col.names=FALSE,row.names=FALSE)

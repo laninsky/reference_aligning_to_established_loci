bcf392fa88f7250ba493a1d36b551e26ad432c6b
Alana Alexander <laninsky@gmail.com>
2016-04-18 10:01:43 -0500

    Update creating_frame.R

--- creating_frame.R
+++ creating_frame.R
@@ -4,9 +4,14 @@ names <- rbind(species,species)
 names <- names[order(names[,1]),]
 names <- names[order(names[,2]),]
 
-names <- t(as.matrix(c("locus","SNP",names[,1])))
+snprecord <- t(as.matrix(c("locus","SNP",names[,1])))
+write.table(snprecord, "full_SNP_record.txt",quote=FALSE, col.names=FALSE,row.names=FALSE)
 
-write.table(names, "full_SNP_record.txt",quote=FALSE, col.names=FALSE,row.names=FALSE)
-write.table(names, "frame_record.txt",quote=FALSE, col.names=FALSE,row.names=FALSE)
+spassignment <- rbind(c("SNP","SNP"),names)
+spassignment <- rbind(c("locus","locus"),spassignment)
+
+spassignment <- t(spassignment)
+
+write.table(spassignment, "frame_record.txt",quote=FALSE, col.names=FALSE,row.names=FALSE)
 
 q()

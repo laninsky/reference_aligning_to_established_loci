acca72d12ff81cb1645bb013d463427df6be572d
Alana Alexander <laninsky@gmail.com>
2016-04-18 10:32:14 -0500

    Update creating_frame.R

--- creating_frame.R
+++ creating_frame.R
@@ -14,4 +14,16 @@ spassignment <- t(spassignment)
 
 write.table(spassignment, "frame_record.txt",quote=FALSE, col.names=FALSE,row.names=FALSE)
 
+individual_no <- dim(spassignment)[2]-2
+uniquespecies <- unique(spassignment[2,3:(individual_no+2)])
+n_uniquespecies <- paste("n_",uniquespecies,sep="")
+k_uniquespecies <- paste("k_",uniquespecies,sep="")
+SNPs_uniquespecies <- paste("SNPs_",uniquespecies,sep="")
+H_uniquespecies <-  paste("H_",uniquespecies,sep="")
+ordered_samples <- unique(spassignment[1,3:(individual_no+2)])
+
+locus_summary <- t(as.matrix(c("locus_ID","bp","n_total",n_uniquespecies, "k_total",k_uniquespecies,"SNPs_total",SNPs_uniquespecies,"H_total",H_uniquespecies,ordered_samples)))
+
+write.table(locus_summary, "locus_summary.txt",quote=FALSE, col.names=FALSE,row.names=FALSE)
+
 q()

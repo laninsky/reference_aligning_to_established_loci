a926ee7db0269f9474805e900b67b97a7052593e
Alana Alexander <laninsky@gmail.com>
2016-04-19 12:32:29 -0500

    Update creating_frame.R

--- creating_frame.R
+++ creating_frame.R
@@ -5,7 +5,10 @@ names <- names[order(names[,1]),]
 names <- names[order(names[,2]),]
 
 snprecord <- t(as.matrix(c("locus","SNP",names[,1])))
+allelerecord <- t(as.matrix(c("locus",names[,1])))
+
 write.table(snprecord, "full_SNP_record.txt",quote=FALSE, col.names=FALSE,row.names=FALSE)
+write.table(allelerecord, "allele_record.txt",quote=FALSE, col.names=FALSE,row.names=FALSE)
 
 spassignment <- rbind(c("SNP","SNP"),names)
 spassignment <- rbind(c("locus","locus"),spassignment)

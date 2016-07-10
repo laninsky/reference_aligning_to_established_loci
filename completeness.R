9cc5a3a4a6362d0c42558d6ed14dc1de7390cb96
Alana Alexander <laninsky@gmail.com>
2016-07-10 16:11:09 -0500

    Create completeness.R

---
+++ completeness.R
@@ -0,0 +1,7 @@
+library(stringr)
+
+propdata <- as.matrix(read.table("proportion.txt",header=FALSE,stringsAsFactors=FALSE))
+filename <- paste("structurefile_step",propdata[2,1],".txt",sep="")
+intable <- as.matrix(read.table(filename,header=FALSE,stringsAsFactors=FALSE))
+
+

3580a0e4151dc82d25e15b16b9d2fbde86ddafa2
Alana Alexander <laninsky@gmail.com>
2016-07-10 16:15:10 -0500

    Update completeness.R

--- completeness.R
+++ completeness.R
@@ -4,4 +4,9 @@ propdata <- as.matrix(read.table("proportion.txt",header=FALSE,stringsAsFactors=
 filename <- paste("structurefile_step",propdata[2,1],".txt",sep="")
 intable <- as.matrix(read.table(filename,header=FALSE,stringsAsFactors=FALSE))
 
+numcols <- dim(intable)[2]
+numtaxa <- dim(intable)[1]
+
+
+
 

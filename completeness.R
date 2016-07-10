3072ad2b3fc9c608fb89e92b7793127bec2428ce
Alana Alexander <laninsky@gmail.com>
2016-07-10 17:09:26 -0500

    Update completeness.R

--- completeness.R
+++ completeness.R
@@ -7,6 +7,15 @@ intable <- as.matrix(read.table(filename,header=FALSE,stringsAsFactors=FALSE))
 numcols <- dim(intable)[2]
 numtaxa <- dim(intable)[1]
 
+numzeroes <- colSums(intable[1:numtaxa,]=="0")
+keepcols <- which(numzeroes<(propdata[1,1]*numtaxa))
 
+intable <- intable[,keepcols]
 
+outfilename <- paste("structurefile_step",propdata[2,1],"_missing_",propdata[1,1],".txt",sep="")
 
+write.table(intable, outfilename,quote=FALSE, col.names=FALSE,row.names=FALSE, append=TRUE)
+
+print(paste("The new structure file has ",(dim(intable)[2]-1)," loci"))
+
+q()

5eecf2b18123b269bc185c363254e59e1adab61e
Alana Alexander <laninsky@gmail.com>
2018-04-25 22:42:28 +1200

    Update modref.R

--- modref.R
+++ modref.R
@@ -31,37 +31,48 @@ sequencepaste <- paste(sequencepaste,intable[j,1],sep="")
 
 to_write[tablelength,1] <- sequencepaste
 
-
+# getting the loci that are present in the to_write file more than once
 locinames <- to_write[(seq(1,(dim(to_write)[1]),2)),1]
-dupllocinames <- locinames[duplicated(locinames)]
+dupllocinames <- unique(locinames[duplicated(locinames)])
 uniquelocinames <- unique(locinames)
 
+# setting up the final matrix to write out to (dealing with the duplicated sequences)
 to_write_final <- matrix(NA,ncol=1,nrow=(length(uniquelocinames)*2))
 
+# If there are duplicated sequences
 if(length(dupllocinames)>0) {
 
+# For each of the duplicate loci names  
 for (i in 1:length(dupllocinames)) {
 tempsequence <- NULL
+  #for each sequence name in to_write
 for (j in (seq(1,(dim(to_write)[1]),2))) {
-if(to_write[j,1]==dupllocinames[i]) {
-tempsequence <- paste(tempsequence,to_write[(j+1),1],sep="")
-}
+  # if the sequence name is in one of the duplicated loci
+  if(to_write[j,1]==dupllocinames[i]) {
+    # then the sequence should be that pasted to the rest of that locus
+    tempsequence <- paste(tempsequence,to_write[(j+1),1],sep="")
+  }
 }
 to_write_final[((i*2)-1),1] <- dupllocinames[i]
 to_write_final[(i*2),1] <- tempsequence
 }
 
+# getting the new starting point for writing the rest of the sequences out to to_write_final  
 i <- (length(dupllocinames)*2)+1
 
+  #for each sequence name in to_write
 for (j in (seq(1,(dim(to_write)[1]),2))) {
-if(!(to_write[j,1] %in% dupllocinames)) {
-to_write_final[i,1] <- to_write[j,1]
-to_write_final[(i+1),1] <- to_write[(j+1),1]
-i <- i+2
-}
-}
+  # if it isn't one of the duplicated guys
+  if(!(to_write[j,1] %in% dupllocinames)) {
+    # then there can be a straight one to one writing of to_write to to_write_final for that sequence
+    to_write_final[i,1] <- to_write[j,1]
+    to_write_final[(i+1),1] <- to_write[(j+1),1]
+    i <- i+2
+  }
+ }
 } else {
-to_write_final <- to_write
+  # If there aren't any duplicated sequences then the final output is the same as previous
+  to_write_final <- to_write
 }
 
 write.table(to_write_final, "temp_alt2.fa",quote=FALSE, col.names=FALSE,row.names=FALSE)

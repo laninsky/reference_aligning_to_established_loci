9cb320bd4f57b0bbeedf80134f61cb77768b102b
Alana Alexander <laninsky@gmail.com>
2018-12-10 11:02:36 +1300

    Update phasing.sh

--- phase_target/phasing.sh
+++ phase_target/phasing.sh
@@ -62,47 +62,51 @@ rm -rf $name.temp*;
 rm -rf $name.reference.fa.*
 rm -rf $name.reference.dict
 
-sed -i 's/\?/N/g' $name.fa;
-sed -i 's/-//g' $name.fa;
-bwa index -a is $name.fa;
-samtools faidx $name.fa;
-$javapath -jar $picard CreateSequenceDictionary R=$name.fa O=$name.dict;
+sed -i 's/\?/N/g' $name.2.reference.fa;
+sed -i 's/-//g' $name.2.reference.fa;
+bwa index -a is $name.2.reference.fa;
+samtools faidx $name.2.reference.fa;
+$javapath -jar $picard CreateSequenceDictionary R=$name.2.reference.fa O=$name.2.reference.fa.dict;
 
 if [ $sequencing == paired ]
 then
-bwa mem -t $numberofcores $name.fa $forward $reverse > temp.sam;
+bwa mem -t $numberofcores ${name}.2.reference.fa $forward $reverse > ${name}.temp.sam;
 fi
 
 if [ $sequencing == single ]
 then
-bwa mem -t $numberofcores $name.fa $forward > temp.sam;
+bwa mem -t $numberofcores ${name}.2.reference.fa $forward > ${name}.temp.sam;
 fi
 
-$javapath -jar $picard AddOrReplaceReadGroups I=temp.sam O=tempsort.sam SORT_ORDER=coordinate LB=rglib PL=illumina PU=phase SM=everyone;
-$javapath -jar $picard MarkDuplicates MAX_FILE_HANDLES=1000 I=tempsort.sam O=tempsortmarked.sam M=temp.metrics AS=TRUE;
-$javapath -jar $picard SamFormatConverter I=tempsortmarked.sam O=tempsortmarked.bam;
-samtools index tempsortmarked.bam;
-# These tools were depreciated in gatk version 4
-#$gatk/gatk -T RealignerTargetCreator -R $name.fa -I tempsortmarked.bam -o tempintervals.list;
-#$gatk/gatk -T IndelRealigner -R $name.fa -I  tempsortmarked.bam -targetIntervals tempintervals.list -o temp_realigned_reads.bam;
-# The -stand_emit_conf 30 option is deprecated in GATK v 3.7 and was removed from this code on the 5-June-2017
-$gatk/gatk HaplotypeCaller -R $name.fa -I tempsortmarked.bam -stand-call-conf 30 -O temp_raw_variants.vcf;
-$javapath -jar $gatk38 -T FindCoveredIntervals -R $name.fa -I tempsortmarked.bam -cov 1 -o temp_covered.list;
-$javapath -jar $gatk38 -T FastaAlternateReferenceMaker -V temp_raw_variants.vcf -R $name.fa -o temp_alt.fa;
+samtools view -@ $numberofcores -b -F 5 -T ${name}.2.reference.fa $name.temp.sam > $name.temp.bam
+# Sorting bam and indexing resulting file
+samtools sort -@ $numberofcores $name.temp.bam > $name.tempsorted.bam
+$javapath -jar $picard MarkDuplicates MAX_FILE_HANDLES=1000 I=$name.tempsorted.bam O=$name.tempsorteddups.bam M=$name.temp.metrics AS=TRUE;
+$javapath -jar $picard AddOrReplaceReadGroups I=$name.tempsorteddups.bam O=$name.tempsorteddupsrg.bam LB=rglib PL=illumina PU=phase SM=everyone;
+
+samtools index -@ $numberofcores $name.tempsorteddupsrg.bam;
+
+# Phasing
+$gatk/gatk HaplotypeCaller -R ${name}.2.reference.fa -I $name.tempsorteddupsrg.bam -stand-call-conf 30 -O $name.temp_raw_variants.vcf;
+
+# nt/nct option doesn't work for these commands
+$javapath -jar $gatk38 -T FindCoveredIntervals -R ${name}.2.reference.fa -I $name.tempsorteddupsrg.bam -cov 1 -o $name.temp_covered.list;
+$javapath -jar $gatk38 -T FastaAlternateReferenceMaker -V $name.temp_raw_variants.vcf -R ${name}.2.reference.fa -L $name.temp_covered.list -o $name.temp_alt.fa;
 
 Rscript modref.R $name.temp_alt.fa
 
-mv $name.fa safe.$name.fa.ref.fa;
-rm -rf $name.*;
-mv temp_alt2.fa $name.1.fa;
-mv safe.$name.fa.ref.fa $name.2.fa;
-rm -rf temp*;
+mv $name.temp.reference.fa $name.1.reference.fa;
+rm -rf $name.temp*;
+rm -rf $name.reference.2.fa.*
+rm -rf $name.reference.2.fa.dict
 
-done
+sed -i 's/\?/N/g' $name.2.reference.fa;
+sed -i 's/-//g' $name.2.reference.fa;
+}
+
 
 for i in `seq 1 $nosamples`;
 do 
-
-for i in `ls *1.fa`;
-do sed -i 's/-//g' $i;
 done
+
+
